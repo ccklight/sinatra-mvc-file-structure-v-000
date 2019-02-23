799b4c5c88de40b6807f79933a8de0829d2996c0
Cheryl K <cherylkelson@icloud.com>
2019-02-23 20:41:52 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -12,3 +12,20 @@ class Dog
   end
   #@@all < dog
 end
+
+********
+class Dog
+  attr_accessor :name, :breed, :age
+  DOGS = []
+  def initialize(name, breed, age)
+    @name = name
+    @breed = breed
+    @age = age
+    DOGS << self
+  end
+
+  def self.all
+    DOGS
+  end
+
+end
